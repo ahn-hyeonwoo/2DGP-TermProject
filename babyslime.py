cec4701bcb58b23828b9aa220adba54f81289db3
ahn-hyeonwoo <90032814+ahn-hyeonwoo@users.noreply.github.com>
2021-11-17 22:14:56 +0900

    make object list
    add game_world.py file

--- babyslime.py
+++ babyslime.py
@@ -1,19 +1,26 @@
 from pico2d import *
+import game_world
 
 class BabySlime: 
     def __init__(self):
-        self.image = load_image('babyslime_walk_1.png')
+        self.image = list(range(5))
+        for i in range(0, 5):
+            self.image[i] = load_image('mobs/babyslime/walk/babyslime_walk_%d.png' % int(i+1)) 
         self.x = 580 // 2
         self.y = 360 // 2
+        self.frame = 0
         
     def draw(self): 
-        self.image.draw(self.x, self.y, 30, 22)
+        self.image[int(self.frame)].draw(self.x, self.y, 30, 22)
         
-    def update(self, will_x, will_y): 
+    def update(self):
+        will_x = game_world.will_x
+        will_y = game_world.will_y 
         x_dist = will_x - self.x
         y_dist = will_y - self.y
         if abs(x_dist) > abs(y_dist): 
-            self.x += (x_dist / abs(x_dist)) * 0.1
+            self.x += (x_dist / abs(x_dist)) * 1.4
         elif abs(x_dist) < abs(y_dist): 
-            self.y += (y_dist / abs(y_dist)) * 0.1
+            self.y += (y_dist / abs(y_dist)) * 1.4
 
+        self.frame = (self.frame + 0.15) % 5
